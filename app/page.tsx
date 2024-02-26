6549dce5a4338edba31670326cb3851760c76c3c
Shin-sibainu <shincode0712@gmail.com>
2024-02-26 10:45:44 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: posts } = await supabase.from("Posts").select();
+  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+}
