d9a42ebd446009a36311c8763022ea831b9ba31f
Shin-sibainu <shincode0712@gmail.com>
2024-02-26 12:04:25 +0900

    oauth provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import AuthButton from "./auth-button";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: posts } = await supabase.from("Posts").select();
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+
+  return (
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </>
+  );
 }
